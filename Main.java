f4baf203382935d1c84a158b0bf7ef48b0ba844d
Bhupendra Rajput <sinnga031@gmail.com>
2026-07-19 10:30:25 +0530

    Updated SumArray

--- Main.java
+++ Main.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Size: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter Elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        System.out.println("After Moving Even Numbers to the Front: ");
+        for (int i = 0; i < j; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
