25f3d0a32da6fdbd1208ddb8972d124610809dbf
Bhupendra Rajput <sinnga031@gmail.com>
2026-07-19 10:55:12 +0530

    Added Bubble Sort in java

--- Main.java
+++ Main.java
@@ -3,26 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter Size: ");
+        System.out.print("enter Size: ");
         int n = sc.nextInt();
         int arr[] = new int[n];
         System.out.println("Enter Elements: ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        int j = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] % 2 == 0) {
-
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                j++;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
-        System.out.println("After Moving Even Numbers to the Front: ");
-        for (int i = 0; i < j; i++) {
+        System.out.println("Sorted Array: ");
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
     }
